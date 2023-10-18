887de2d7b68be8551f620b4f574df662b9e0b91b
danizavtz <daniellucena@yahoo.com.br>
2023-10-18 20:04:49 -0300

    add aws-ses service integration

--- app/controllers/contact-controller.js
+++ app/controllers/contact-controller.js
@@ -1,13 +1,16 @@
 import layout from '../views/layout.js';
 import Contato from '../models/contato.js';
 import sucesso from '../views/sucesso.js';
+import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2";
+const config = { region: 'us-east-1' } // configure a region em que está configurado o SES
+const client = new SESv2Client(config);
 
 export default {
   index(req, res) {
     return res.send(layout({ contato: new Contato() }));
   },
 
-  sendMail(req, res) {
+  async sendMail(req, res) {
     const contato = new Contato(req.body)
     if (contato.email.trim() === '') {
       contato.errors.email.push('E-mail não pode ser vazio')
@@ -18,9 +21,43 @@ export default {
     if (contato.message.trim() === '') {
       contato.errors.message.push('Mensagem não pode ser vazio')
     }
-    if (contato.errors && (contato.errors.email.length > 0 || contato.errors.message.length > 0 || contato.errors.title.length > 0 || contato.errors.service.length > 0)) {
+    if (contato.errors && (contato.errors.email.length > 0 || contato.errors.message.length > 0 || contato.errors.title.length > 0)) {
       return res.send(layout({ contato: contato }))
+    } //até aqui faz validação dos campos obrigatórios, renderiza o form com as mensagens de erro e os dados do formulário
+
+    const input = { // SendEmailRequest
+      FromEmailAddress: "contato@danizavtz.com.br",
+      ReplyToAddresses: [
+        "daniellucena@yahoo.com.br",
+      ],
+      Destination: { // Destination
+          ToAddresses: [ // EmailAddressList
+            "daniellucena@yahoo.com.br",
+          ]
+      },
+      Content: { // EmailContent
+        Simple: { // Message
+          Subject: { // Content
+            Data: req.body.title, // required
+            Charset: "UTF-8",
+          },
+          Body: { // Body
+            Text: {
+              Data: `Mensagem de: ${req.body.email}, \nMensagem: ${req.body.message}`, // required
+              Charset: "UTF-8",
+            }
+          }
+        }
+      }
+    };
+    const command = new SendEmailCommand(input);
+    try {
+        await client.send(command);
+        res.send(sucesso())
+    } catch (err) {
+        console.log(err); //esta linha não é necessária, é utilizada apenas para debug
+        contato.errors.service.push('Houve um erro durante o envio')
+        return res.send(layout({ contato: contato }))
     }
-    return res.send(sucesso())
   }
 };
